3fa5eab49a246039bca62c35e49eb29ed8b9c0d2
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:38:25 -0300

    atv09

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+double valorA = 0.0;
+double valorB = 0.0;
+double valorFinal = 0;
+
+
+WriteLine("Escreva o valor 1: ");
+valorA = Convert.ToInt32(ReadLine());
+
+WriteLine("Escreva o valor 2: ");
+valorB = Convert.ToInt32(ReadLine());
+
+if (valorA <= 0 || valorB <= 0 )
+{
+    WriteLine("Valores = 0 não se dividem");
+}
+else
+{
+    valorFinal = valorA / valorB;
+}
+
+WriteLine($"O resultado da divisão é: {valorFinal}");
+
